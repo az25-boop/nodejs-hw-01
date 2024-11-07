e4089a838183ba4b629e231b14c19784cdade4a7
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-11-07 13:56:56 +0200

    js

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,4 @@
+import { fileURLToPath } from 'node:url';
+
 export const PATH_DB =
+  fileURLToPath(new URL('../bd', import.meta.url)) + '/db.json';
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts);
+  try {
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('ok');
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};

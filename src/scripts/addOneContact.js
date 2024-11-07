5bd1c3e18d6dbff6f9387d3a6df832d793fcfdf0
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-11-07 13:03:18 +0200

    js

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { readContacts } from '../utils/createFakeContact';
+import { writeContacts } from '../utils/createFakeContact';
+
+export const addOneContact = async () => {
+  let contacts = JSON.parse(await readContacts());
+  contacts.push(createFakeContact());
+  await writeContacts(contacts);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { writeContacts } from '../utils/writeContacts';
+
+const generateContacts = async (number) => {
+  let data = [];
+  for (let i = 0; i < number; i++) {
+    data.push(createFakeContact());
+  }
+  await writeContacts(data);
+};
 
 generateContacts(5);
